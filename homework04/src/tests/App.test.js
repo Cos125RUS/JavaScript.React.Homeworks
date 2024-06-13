863f4a171bb97a9ab65c24085c290e57fb86ca1e
Valerii_Panov <val250640@mail.ru>
2024-06-13 20:25:47 +1000

    add waitFor, but tests in headers not working

--- homework04/src/tests/App.test.js
+++ homework04/src/tests/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Home Page/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+    render(<App/>);
+    await waitFor(() => {
+        const linkElement = screen.getByText(/Home Page/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 });
--- homework04/src/tests/Header.test.js
+++ homework04/src/tests/Header.test.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {getByText, render, screen} from "@testing-library/react";
+import {getByText, render, screen, waitFor} from "@testing-library/react";
 import Header from "../components/Header";
 
 
-test("render header", () => {
+test("render header", async () => {
     render(<Header/>);
-    expect(screen.getByText(/About us/i)).toBeInTheDocument();
+    await waitFor(() => {
+        const findEl = screen.getByText(/About us/i);
+        expect(findEl).toBeInTheDocument();
+    });
 });
 
-test("links count test", () => {
+test("links count test", async () => {
     render(<Header/>);
-    // const headerEl = document.querySelector("header");
-    // expect(headerEl.childElementCount).toBe(2);
+    // await waitFor(() => {
+        // const headerEl = document.querySelector("header");
+        // expect(headerEl.childElementCount).toBe(2);
+    // });
 });
 
 test("header snapshot test", () => {
