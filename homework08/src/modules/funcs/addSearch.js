8e0bafcb4a4a7acf732b783a1ccce68d9f02a183
Valerii_Panov <val250640@mail.ru>
2024-06-28 10:59:35 +1000

    done filters

--- homework08/src/modules/funcs/addSearch.js
+++ homework08/src/modules/funcs/addSearch.js
@@ -1,5 +1,4 @@
-
-const addSearch = (navigate, location, checkbox, option, ) => {
+const addSearch = (navigate, location, checkbox, option,) => {
     const query = new URLSearchParams(location.search);
 
     if (checkbox.checked) {
@@ -18,7 +17,36 @@ const addSearch = (navigate, location, checkbox, option, ) => {
             navigate(location.pathname + `?${option}=${query.getAll(option)}`);
         }
     } else {
-
+        const oldSearch = query.getAll(option)[0];
+        const values = query.getAll(option)[0].split(',');
+        values.splice(values.indexOf(checkbox.id), 1);
+        query.delete(option);
+        values.forEach(value => query.append(option, value));
+        let otherKeys = 0;
+        for (const key of query.keys()) {
+            otherKeys++;
+        }
+        if (!otherKeys) {
+            if (query.get(option)) {
+                navigate(location.pathname + `?${option}=${query.getAll(option)}`);
+            } else {
+                navigate(location.pathname);
+            }
+        } else {
+            let newSearch = '';
+            if (query.get(option)) {
+                newSearch = location.search.replace(`${option}=${oldSearch}`, `${option}=${query.getAll(option)}`);
+            } else {
+                const searchItems = location.search.replace('?', '').split('&');
+                const resultItems = searchItems.filter(item => item !== `${option}=${oldSearch}`);
+                newSearch = '?';
+                for (let i = 0; i < resultItems.length - 1; i++) {
+                    newSearch += `${resultItems[i]}&`;
+                }
+                newSearch += `${resultItems[resultItems.length - 1]}`;
+            }
+            navigate(location.pathname + newSearch);
+        }
     }
 };
 
